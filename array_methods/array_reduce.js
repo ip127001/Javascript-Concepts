43f7fa7b2a20e4ee9fd478fe944c74812b439e5c
Rohit Kumawat <rkumawat125@gmail.com>
2018-09-17 22:46:22 +0530

    array reduce

---
+++ array_methods/array_reduce.js
@@ -0,0 +1,20 @@
+var votes = [
+  "angular",
+  "angular",
+  "react",
+  "vue",
+  "ember",
+  "vanilla",
+  "angular"
+];
+
+var result = votes.reduce((prev, next) => {
+  if (!prev[next]) {
+    prev[next] = 1;
+  } else {
+    prev[next] = prev[next] + 1;
+  }
+  return prev;
+}, {});
+
+console.log(result);
