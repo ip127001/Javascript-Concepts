9b2cb8a07613d256a0626dba3eab07f2c0105e1d
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-09 10:21:34 +0530

    delete functionality axios

--- http-ajax-react/src/containers/Blog/Blog.js
+++ http-ajax-react/src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedId: null
+        selectedId: null,
+        error: false
     }
 
     selectedPost = (id) => {
@@ -18,7 +19,7 @@ class Blog extends Component {
 
     componentDidMount() {
         console.log('[Blog.js] componentDidMount')
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/postssssss')
         .then((response) => {
             const updatedPosts = response.data.slice(0, 4).map(post => {
                 return {
@@ -29,13 +30,22 @@ class Blog extends Component {
             this.setState({posts: updatedPosts})
             //console.log(response);
         })
+        .catch(err => {
+            this.setState({error: true})
+        });
     }
 
     render () {
         console.log('[blog.js] render')
-        const posts = this.state.posts.map((post) => {
-            return <Post key={post.id} author={post.author} title={post.title} clicked={() => {this.selectedPost(post.id)}} />;
-        });
+        let posts = <p style={{textAlign: 'center'}}>Error occured!!</p>;
+        if(!this.state.error) {
+            posts = this.state.posts.map((post) => {
+                return <Post key={post.id} 
+                            author={post.author} 
+                            title={post.title} 
+                            clicked={() => {this.selectedPost(post.id)}} />;
+            });
+        } 
 
         return (
             <div>
