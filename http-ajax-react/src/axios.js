7e47617a14eb2c6ea3c70821d6a191633ba9f5dc
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-12 18:46:03 +0530

    created and used axios instance

---
+++ http-ajax-react/src/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKE FROM INSTANCE';  // overwrite the default header
+
+export default instance;
+
