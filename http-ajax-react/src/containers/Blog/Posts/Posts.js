9fab411e5f9ea39d7b8d56efc0b41d1066e0e818
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-17 10:51:58 +0530

    passing route parameters and extracting them

--- http-ajax-react/src/containers/Blog/Posts/Posts.js
+++ http-ajax-react/src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
+import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -38,10 +39,14 @@ class Posts extends Component {
         let posts = <p style={{textAlign: 'center'}}>Error occured!!</p>;
         if(!this.state.error) {
             posts = this.state.posts.map((post) => {
-                return <Post key={post.id} 
-                            author={post.author} 
-                            title={post.title} 
-                            clicked={() => {this.selectedPost(post.id)}} />;
+                return (
+                <Link to={'/' + post.id} key={post.id}>
+                    <Post 
+                        author={post.author} 
+                        title={post.title} 
+                        clicked={() => {this.selectedPost(post.id)}} />
+                </Link>
+                );    
             });
         } 
 
