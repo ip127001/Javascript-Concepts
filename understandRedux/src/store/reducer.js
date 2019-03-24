ca0e53b9d9d6e7db76d627e28fb6f492ca17b4d5
Rohit Kumawat <rkumawat125@gmail.com>
2019-03-24 18:38:07 +0530

    dispatching actions(args) from within components

--- understandRedux/src/store/reducer.js
+++ understandRedux/src/store/reducer.js
@@ -1,9 +1,28 @@
 const initailState = {
-    counter: 0
+    counter: 0,
 }
 
 const reducer = (state = initailState, action) => {
-    return state;
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT': 
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.val
+            }
+        case 'SUBTRACT':
+            return {
+                counter: state.counter - action.val
+            }
+        default:
+            return state;
+    }
 }
 
 export default reducer;
